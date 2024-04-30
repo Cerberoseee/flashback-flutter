06b2bd5a607f451cf0abafce1b9726f593d60b4d
Cerberose <tranlethaison123@gmail.com>
2024-04-30 10:47:08 +0700

    search community

--- lib/src/screens/communities/communities_search_view.dart
+++ lib/src/screens/communities/communities_search_view.dart
@@ -1,6 +1,5 @@
 import 'package:flutter/material.dart';
 import 'package:flutter_final/src/widgets/vocab_list_widget.dart';
-import 'package:google_fonts/google_fonts.dart';
 
 class CommunitySearchView extends StatefulWidget {
   const CommunitySearchView({super.key});
